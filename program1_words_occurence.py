75d55e74c9209a0eb144f536d08b4bc4c8ef4483
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2024-01-22 21:57:41 -0600

    Update program1_words_occurence.py

--- program1_words_occurence.py
+++ program1_words_occurence.py
@@ -1,5 +1,7 @@
 sentence = "which is better python 2 or python 3"
+# Split the sentece in to the words
 words = sentence.split(" ")
-print("words,", words)
+#print("words,", words)
+# using dictionary comprehension iterate all words and increment count for each word
 new_dict= {x:words.count(x) for x in words}
 print("output", new_dict)
